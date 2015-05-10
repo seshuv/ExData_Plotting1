b14ea886db8d685018934b2c4495cfcd0410d465
Seshu Vavilikolanu <seshuvavilikolanu@Seshu-Vavilikolanus-MacBook-Pro.local>
2015-05-10 00:28:53 -0500

    Commiting plot1 code and image'

---
+++ plot1.R
@@ -0,0 +1,16 @@
+powerConsumptionData <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", stringsAsFactors=FALSE)
+
+library(dplyr)
+
+powerConsumptionData$DateTime <- strptime(paste(powerConsumptionData$Date, powerConsumptionData$Time), format="%d/%m/%Y %H:%M:%S")
+
+powerConsumptionData$DateTime <- as.Date(powerConsumptionData$DateTime, format = "%d/%m/%Y %H:%M:%S")
+data <- filter(powerConsumptionData, DateTime >= as.Date("2007-02-01 00:00:00"), DateTime < as.Date("2007-02-03 00:00:00"))
+
+rm(powerConsumptionData)
+
+#plot 1
+hist(data$Global_active_power, col= "red", xlab = "Global Active Power (kilowatts)")
+
+#plot 2
+#plot(data$Global_active_power, data$Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
