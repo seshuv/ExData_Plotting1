11fa574550277489d220b01a0eaeca51f29addbd
Seshu Vavilikolanu <seshuvavilikolanu@Seshu-Vavilikolanus-MacBook-Pro.local>
2015-05-10 01:20:02 -0500

    plot2.  Had to spend quite some time on the week day lables

---
+++ plot2.R
@@ -0,0 +1,17 @@
+library(dplyr)
+powerConsumptionData <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", stringsAsFactors=FALSE)
+
+## Need to convert the DateTime to POSIXct to get the proper plot with week days
+## Had to change the way of filtering the data from plot1 to do it as POSIXct
+## Note to self: Look at lubridate later
+powerConsumptionData$Date <- as.Date(powerConsumptionData$Date, format="%d/%m/%Y")
+data <- subset(powerConsumptionData, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+datetime <- paste(as.Date(data$Date), data$Time)
+data$DateTime <- as.POSIXct(datetime)
+rm(powerConsumptionData)
+
+#plot 2
+plot(data$Global_active_power~data$DateTime, xlab="",ylab="Global Active Power (kilowatts)", type="l")
+
+dev.copy(png,'plot2.png')
+dev.off()
